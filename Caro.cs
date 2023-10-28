212201b0a7180369c521036b788c36b43490cb71
Fogashi <mateus.luna01@fatec.sp.gov.br>
2023-10-27 23:58:06 -0300

    Adicionar arquivos de projeto.

---
+++ Caro.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AleProjeto
+{
+    class Caro
+    {
+        private string marca;
+        private int ano;
+        private double motor1;
+        private int Velocidade;
+        private double gasolina = 5.29;
+        private int estado;
+        private bool ligado;
+        private double Precogasolina;
+
+        public Caro(string marca, int ano, double motor1, int velocidade, double gasolina, int estado, bool ligado, double preogasolina)
+        {
+            this.marca = marca;
+            this.ano = ano;
+            this.motor1 = motor1;
+            this.Velocidade = velocidade;
+            this.gasolina = gasolina;
+            this.estado = estado;
+            this.ligado = ligado;
+            this.Precogasolina = preogasolina;
+        }
+
+
+        public int MostrarVelocidade()
+        {
+            int mostrarv = this.Velocidade;
+            return mostrarv;       
+        }
+
+
+        public void Verificar(int verificacao)
+        {
+            if (verificacao == 1)
+            {
+                this.ligado = true;
+
+            }
+            else
+            {
+                 this.ligado = false;
+
+            }
+
+        }
+
+
+        public int Acelerar(int veloo)
+        {
+            if(veloo <180 && veloo > 0)
+            {
+            if(this.ligado == true)
+            {
+            return this.Velocidade += veloo;
+
+            }
+            return 0;
+
+            }
+            return 0;
+        }
+
+
+        public double gasosa(double litros)
+        {
+            this.Precogasolina = this.gasolina * litros;
+            return this.Precogasolina;
+        }
+
+
+
+        public void Freiar()
+        {
+            this.Velocidade = 0;
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AleProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string marca;
+            int ano;
+            double motor;
+            Console.WriteLine("digite a marca do carro");
+            marca = Console.ReadLine();
+            Console.WriteLine("digite o ano do carro");
+            ano = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite o motor do carro");
+            motor = double.Parse(Console.ReadLine());
+            Console.WriteLine("digite 1 para ligar o carro");
+            int teste = int.Parse(Console.ReadLine());
+
+            Caro x = new Caro(marca, ano, motor, 0, 5.29, 0,true,0);
+
+            x.Verificar(teste);
+
+            Console.WriteLine("digite a velocidade para acelerar o carro");
+            int velocidade1 = int.Parse(Console.ReadLine());
+            int velo;
+            velo = x.Acelerar(velocidade1);
+            Console.WriteLine(velo);
+            Console.WriteLine("digite 1 para freiar");
+            Console.WriteLine("digite 2 para acelerar mais");
+            int codicao2 = int.Parse(Console.ReadLine());
+
+
+            if (codicao2 == 1)
+            {
+                x.Freiar();
+                Console.WriteLine("velocidade do carro {0}", x.MostrarVelocidade());
+            }
+            else
+            {
+                
+                Console.WriteLine("digite novamente a velocidade para acelerar");
+                int velocidade2 = int.Parse(Console.ReadLine());
+                int velo2;
+                velo2  = x.Acelerar(velocidade2);
+                Console.WriteLine(velo2);
+            }
+
+            Console.WriteLine("quantos litros de gasolina você deseja?");
+            double ltgasolina = double.Parse(Console.ReadLine());
+            double preco = x.gasosa(ltgasolina);
+            Console.WriteLine(preco);
+
+
+        }
+    }
+}
